98114994e9e3869170ade212a9f3c43be5990b64
tolstosheev17 <dane4ka.06@mail.ru>
2024-12-13 23:37:15 +0300

    2 components now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './header/header.component';
+import {HousingLocationComponent} from './housing-location/housing-location.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, HousingLocationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'HW-7';
+  title = 'homes';
 }
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.scss'
+})
+export class HousingLocationComponent {
+
+}
